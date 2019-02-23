d7724ee455e5feceb1fb35d2d61fdd38a9897472
lixianzhuang <lixianzhuang@inspur.com>
2019-02-23 08:46:15 +0800

    add searchManagedEntity method into InventoryNavigator

--- src/main/java/com/vmware/vim25/mo/InventoryNavigator.java
+++ src/main/java/com/vmware/vim25/mo/InventoryNavigator.java
@@ -1,10 +1,10 @@
 package com.vmware.vim25.mo;
 
+import java.lang.reflect.Method;
 import java.rmi.RemoteException;
 
 import com.vmware.vim25.*;
 import com.vmware.vim25.mo.util.*;
-import com.vmware.vim25.mo.util.PropertyCollectorUtil;
 
 public class InventoryNavigator
 {
@@ -166,4 +166,96 @@ public class InventoryNavigator
     return null;
   }
 
+  public ManagedEntity searchManagedEntity(ManagedObjectReference mor,
+	         String propertyName) throws InvalidProperty, RuntimeFault, RemoteException {
+	      ObjectContent[] ocs = retrieveObjectContents(mor,propertyName,true);
+
+		if (ocs==null || ocs.length == 0) 
+		{
+			return null;
+		}
+
+		for (int i = 0; i < ocs.length; i++) 
+		{
+			DynamicProperty[] propSet = ocs[i].getPropSet();
+			
+			if (propSet.length > 0) 
+			{
+				String nameInPropSet = (String) propSet[0].getName();
+				if(propertyName.equalsIgnoreCase(nameInPropSet))
+				{
+					ManagedObjectReference morr = ocs[i].getObj();
+					return MorUtil.createExactManagedEntity(rootEntity.getServerConnection(), morr);
+				}
+			}
+		}
+		return null;
+	}
+
+	private ObjectContent[] retrieveObjectContents( ManagedObjectReference mor,
+	         String propertyName,boolean recurse) throws InvalidProperty, RuntimeFault, RemoteException
+	{
+
+		PropertyCollector pc = rootEntity.getServerConnection().getServiceInstance().getPropertyCollector();
+	
+		if (recurse && selectionSpecs==null) 
+		{
+		  AboutInfo ai = rootEntity.getServerConnection().getServiceInstance().getAboutInfo();
+		  
+		  /* The apiVersion values in all the shipped products
+		  "2.0.0"    VI 3.0
+		  "2.5.0"    VI 3.5 (and u1)
+		  "2.5u2"   VI 3.5u2 (and u3, u4)
+		  "4.0"       vSphere 4.0 (and u1)
+		  "4.1"       vSphere 4.1
+		  "5.0"       vSphere 5.0
+   ******************************************************/
+			if(ai.apiVersion.startsWith("4") || ai.apiVersion.startsWith("5"))
+			{
+			  selectionSpecs = PropertyCollectorUtil.buildFullTraversalV4();
+			}
+			else
+			{
+			  selectionSpecs = PropertyCollectorUtil.buildFullTraversal();
+			}
+		}
+	
+		
+		PropertySpec proSpec = new PropertySpec();
+		proSpec.setType( mor.getType() );
+		proSpec.setPathSet( new String[]{propertyName} );
+		proSpec.setAll(new Boolean( propertyName == null || propertyName.equals("") ));
+	
+		ObjectSpec os = new ObjectSpec();
+		os.setObj( mor );
+		os.setSkip(Boolean.FALSE);
+		os.setSelectSet(selectionSpecs);
+		
+		PropertyFilterSpec spec = new PropertyFilterSpec();
+		spec.setObjectSet(new ObjectSpec[] { os });
+		spec.setPropSet(new PropertySpec[] { proSpec });
+
+		return pc.retrieveProperties(new PropertyFilterSpec[] { spec } );
+	}
+
+	   /**
+	    * Determines of a method 'methodName' exists for the Object 'obj'
+	    * @param obj The Object to check
+	    * @param methodName The method name
+	    * @param parameterTypes Array of Class objects for the parameter types
+	    * @return true if the method exists, false otherwise
+	    */
+	   @SuppressWarnings("rawtypes")
+	boolean methodExists(Object obj, 
+	         String methodName, 
+	         Class[] parameterTypes) {
+	      boolean exists = false;
+	      try {
+	         Method method = obj.getClass().getMethod(methodName, parameterTypes);
+	         if (method != null) {
+	            exists = true;
+	         }
+	      } catch(Exception e){}
+	      return exists;
+	   }
 }
